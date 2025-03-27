0bd04a75ea96c8fb0b3537beaf8e0bb0f77d4446
oshanQQ <onoyama.shodai@classmethod.jp>
2025-03-27 18:06:48 +0900

    「1MB」から「1KB」へコメントを修正

--- src/app.py
+++ src/app.py
@@ -53,7 +53,7 @@ def send_to_firehose(stream_name, data):
 
 def lambda_handler(event, context):
     try:
-        # 各ストリーム用に10MBのデータを生成（合計で50MB程度）
+        # 各ストリーム用に1KBのデータを生成（合計で5KB程度）
         for stream in STREAMS:
             print(f"Generating and sending data to {stream}")
             data = generate_test_data(1)  # 1KBのデータ生成
